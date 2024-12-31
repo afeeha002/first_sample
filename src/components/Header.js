7724c6711aa40d92232d9fccdf224d919f9a9455
afeeha <afeehahamza@gmail.com>
2024-12-31 12:41:53 +0530

    updated

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Button } from 'react-bootstrap';
+const Header = () => {
+    return (
+        <div>
+            {/* <h1 style={{color:"white", backgroundColor: 'darkblue',margin:0,height:100,display:'flex',alignItems:'center'}}>My Simple React App</h1> */}
+
+            <h1>My Simple React App</h1>
+
+        </div>
+    )
+}
+
+export default Header
